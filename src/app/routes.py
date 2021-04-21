73ecae0c8276850b71c7d16092b21aadd73f25c3
Vincent Onyango <verencelola@icloud.com>
2021-04-21 15:37:39 +0300

    add endpint for inputing data

--- src/app/routes.py
+++ src/app/routes.py
@@ -6,11 +6,13 @@ from flask import Blueprint, Response, request, jsonify
 bp = Blueprint("index", __name__, url_prefix="/")
 
 
-@bp.route("/", methods=("GET",))
+@bp.route("/", methods=("POST",))
 def english_words():
-    if request.method == "GET":
+    if request.method == "POST":
+        letters = request.get_json()["letters"]
+
         dictionary = enchant.Dict("en_US")
-        checker = WordChecker(dictionary=dictionary, letters="cipth")
+        checker = WordChecker(dictionary=dictionary, letters=letters)
         english_words = checker.find_english_words()
 
         return jsonify(english_words)
